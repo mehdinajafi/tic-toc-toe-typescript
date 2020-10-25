143f91b76874f77de228d0083397a68e92034f49
Mehdi <mehdinajafi20.mn@gmail.com>
2020-10-25 22:53:31 +0330

    Config prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from "react"
 //import styled from 'styled-components'
-import "./App.css";
+import "./App.css"
 
 function App() {
-  return <div></div>;
+  return <div></div>
 }
 
-export default App;
+export default App
