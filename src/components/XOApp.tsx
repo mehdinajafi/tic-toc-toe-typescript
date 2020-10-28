9879e16c0d728252f72325fc122981506adc8649
Mehdi <mehdinajafi20.mn@gmail.com>
2020-10-28 16:46:18 +0330

    Start button

--- src/components/XOApp.tsx
+++ src/components/XOApp.tsx
@@ -1,45 +1,83 @@
 import React, { useState } from "react"
 import styled from "styled-components"
+import { motion } from "framer-motion"
 import Board, { Winner } from "./views/Game/Board"
 import Reset from "./views/Reset/Reset"
 import Start from "./views/Start/Start"
 
-const Container = styled.div`
-  width: 500px;
-  height: 500px;
-  padding: 1em;
-  background: #fff;
+// Start styles
+const variants = {
+  game: {
+    background: "#fff",
+    opacity: 1,
+    scale: 1,
+    width: "500px",
+    height: "500px",
+    transition: { type: "spring", duration: 0.8 },
+  },
+  start: {
+    background: "transparent",
+    opacity: 1,
+    scale: 1,
+    width: "200px",
+    height: "100px",
+    transition: { type: "spring", duration: 0.8 },
+  },
+  hidden: { opacity: 0, scale: 0.8 },
+}
+
+const Container = styled(motion.div).attrs(() => ({
+  initial: "hidden",
+  variants,
+}))`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   border-radius: 1em;
-  @media (max-width: 680px) {
-    width: 300px;
-    height: 300px;
-  }
-  @media (max-width: 360px) {
-    width: 200px;
-    height: 200px;
-  }
+  padding: 1em;
 `
 
+const AppTitle = styled.div`
+  color: #fff;
+  font-size: 5em;
+  font-weight: 700;
+  @media (max-width: 680px) {
+    font-size: 4em;
+  }
+  @media (max-width: 360px) {
+    font-size: 3em;
+  }
+`
+// End styles
+
+// types
 type GameState = "start" | "game" | "reset"
 
+//component
 function XOApp() {
-  const [gameState, setGameState] = useState<GameState>("game")
-  const [winner, setWinner] = useState<Winner>("")
+  const [gameState, setGameState] = useState<GameState>("start")
+  const [, setWinner] = useState<Winner>("")
+  const onGameStart = () => {
+    setGameState("game")
+  }
 
   const onGameEnd = (winner: Winner) => {
     setGameState("reset")
     setWinner(winner)
   }
   return (
-    <Container>
-      {
+    <React.Fragment>
+      <AppTitle>Tic Tac Toe</AppTitle>
+      <Container animate={gameState}>
         {
-          start: <Start />,
-          game: <Board onGameEnd={onGameEnd} />,
-          reset: <Reset />,
-        }[gameState]
-      }
-    </Container>
+          {
+            start: <Start onGameStart={onGameStart} />,
+            game: <Board onGameEnd={onGameEnd} />,
+            reset: <Reset />,
+          }[gameState]
+        }
+      </Container>
+    </React.Fragment>
   )
 }
 
--- src/components/views/Start/Start.tsx
+++ src/components/views/Start/Start.tsx
@@ -1,7 +1,28 @@
 import * as React from "react"
+import styled from "styled-components"
 
-const Start = () => {
-  return <div>Start</div>
+const StartButton = styled.button`
+  background: #fff;
+  color: #2c3e50;
+  font-family: "Varela Round";
+  font-size: 2em;
+  padding: 0.5em;
+  border: none;
+  border-radius: 0.2em;
+  transition: transform 0.2s ease-in-out;
+  outline: none;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+  }
+`
+
+type StartProps = {
+  onGameStart(): void
+}
+
+const Start: React.FC<StartProps> = ({ onGameStart }) => {
+  return <StartButton onClick={() => onGameStart()}>Start</StartButton>
 }
 
 export default Start
